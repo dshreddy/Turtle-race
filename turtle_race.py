90085bdd78b83537d7b81760ca11130cf6ad47dd
dshreddy <127737097+dshreddy@users.noreply.github.com>
2023-03-15 13:57:15 +0530

    Update turtle_race.py

--- turtle_race.py
+++ turtle_race.py
@@ -9,12 +9,12 @@ from turtle import Turtle, Screen
 sc = Screen()
 sc.setup(width=800, height=600)
 
+
 # Define the main game function
 def game():
-
     # Define a list of colors for the turtles to choose from
     colors = ["red", "blue", "green", "yellow", "orange", "pink", "violet", "black", "brown"]
-    
+
     # Create a list of five turtles and set their properties
     t = [0 for i in range(5)]
     for i in range(5):
@@ -32,7 +32,7 @@ def game():
 
     # Define a list of allowed values for the user's bet
     allowed_values = ['0', '1', '2', '3', '4']
-    
+
     # Prompt the user to enter a bet until they enter a valid number
     user_bet = sc.textinput(title="Make your bet", prompt="Which turtle will win the race ? (Enter a number [0,4])")
     while user_bet not in allowed_values:
@@ -47,7 +47,8 @@ def game():
         t[4].forward(random.randint(0, 10))
 
     # Run the race until one of the turtles reaches a certain point on the screen
-    while t[0].pos()[0] < 360 and t[1].pos()[0] < 360 and t[2].pos()[0] < 360 and t[3].pos()[0] < 360 and t[4].pos()[0] < 360:
+    while t[0].pos()[0] < 360 and t[1].pos()[0] < 360 and t[2].pos()[0] < 360 and t[3].pos()[0] < 360 and t[4].pos()[
+        0] < 360:
         race()
 
     # Create a list of tuples containing the turtles' final positions and their index numbers
@@ -63,17 +64,17 @@ def game():
     res.reverse()
 
     # Determine the winner(s) of the race
-    if res[0][0]!=res[1][0]:
+    if res[0][0] != res[1][0]:
         pr = '''Press 1 to play again, any other key to exit
-        '''+"Winner(s) ="+str(res[0][1])
-        else:
+        ''' + "Winner(s) =" + str(res[0][1])
+    else:
         pr = '''Press 1 to play again, any other key to exit
         ''' + "Winner(s) =" + str(res[0][1])
         i = 1
         while i <= 4 and res[i][0] == res[0][0]:
-            pr += " "+str(res[i][1])
+            pr += " " + str(res[i][1])
             i += 1
-    
+
     # Print the game result on the screen and ask if the user wants to play again
     if t[int(user_bet)].pos()[0] >= 360:
 
@@ -90,4 +91,3 @@ def game():
 
 # First game call
 game()
-
